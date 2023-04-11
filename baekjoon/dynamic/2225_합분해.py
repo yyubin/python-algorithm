300365f94b30a0c511483c0e481e939e90d41d56
yyubin <hazing120@gmail.com>
2023-04-11 22:15:25 +0900

    0411 g5..g2랜디 1문제

---
+++ baekjoon/dynamic/2225_합분해.py
@@ -0,0 +1,15 @@
+import sys
+n, k = map(int, sys.stdin.readline().split())
+
+dp = [[0] * 201 for _ in range(201)]
+
+for i in range(201):
+    dp[1][i] = 1
+    dp[2][i] = i + 1
+
+for i in range(2, 201):
+    dp[i][1] = i
+    for j in range(2, 201):
+        dp[i][j] = dp[i][j-1] + dp[i-1][j]
+
+print(dp[k][n] % 1000000000)
