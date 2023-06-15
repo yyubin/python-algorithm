bbd8e89fcec68c2ed5e719b74ad2a80c2eda692e
yyubin <hazing120@gmail.com>
2023-06-16 02:01:24 +0900

    0615 dp 1sol

---
+++ baekjoon/LIS/dynamic/11055_가장큰증가하는부분수열.py
@@ -0,0 +1,14 @@
+import sys
+n = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().rstrip().split()))
+d = [0] * n
+
+for i in range(n):
+    for j in range(i):
+        if a[i] > a[j] and d[i] < d[j]:
+            d[i] = d[j]
+    d[i] += a[i]
+
+print(max(d))
+
+
