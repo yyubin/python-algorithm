56a0f903e377df6a1b6e56e6f0a935bc6f2e46e3
yyubin <hazing120@gmail.com>
2024-02-16 02:04:23 +0900

    0216 note

---
+++ baekjoon/greedy/1107_리모컨(미).py
@@ -0,0 +1,5 @@
+import sys
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+li = list(map(int, sys.stdin.readline().split()))
+
---
+++ baekjoon/note/0216.py
@@ -0,0 +1,7 @@
+a = input().rstrip()
+b = input().rstrip()
+
+if len(a) < len(b):
+    print("no")
+else:
+    print("go")
