1b9f19744c6113cb9b1a77270f60d7fd5dbbf59a
yyubin <hazing120@gmail.com>
2026-04-22 23:09:32 +0900

    solving outstanding problems complete

--- programmers/implemention/방금그곡.py
+++ programmers/implemention/방금그곡.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 def solution(m, musicinfos):
 
     d = defaultdict(list)
-    for i, musicinfo in enumerate(musicinfos):
+    for idx, musicinfo in enumerate(musicinfos):
         start, end, name, melody = musicinfo.split(",")
         start_hour, start_min = map(int, start.split(":"))
         end_hour, end_min = map(int, end.split(":"))
@@ -11,31 +11,66 @@ def solution(m, musicinfos):
         end = end_hour * 60 + end_min
         time = end - start
 
-        n = len(melody)
-        tmp = time // n
-        if time % n != 0:
-            tmp += 1
+        last = melody[0]
+        melody_list = []
+        for i in range(1, len(melody)):
+            if melody[i] == "#":
+                melody_list.append(last + "#")
+                last = ""
+            elif last == "":
+                last = melody[i]
+            else:
+                melody_list.append(last)
+                last = melody[i]
 
-        d[name] = [time, melody*tmp, i]
+        if last != "":
+            melody_list.append(last)
+        if last == "#":
+            t = melody_list.pop()
+            melody_list.append(t+"#")
+
+        n = len(melody_list)
+
+        if n > time:
+            melody = melody_list[:time]
+        else:
+            mul = time // n
+            mod = time % n
+
+            melody = melody_list * mul
+            melody += melody_list[:mod]
+
+        d[name] = [time, ','.join(melody)+',', idx]
 
     able = []
 
+    m_list = []
+    last = m[0]
+    for i in range(1, len(m)):
+        if m[i] == "#":
+            m_list.append(last + "#")
+            last = ""
+        elif last == "":
+            last = m[i]
+        else:
+            m_list.append(last)
+            last = m[i]
+
+    if last != "":
+        m_list.append(last)
+    if last == "#":
+        t = m_list.pop()
+        m_list.append(t + "#")
+
+    m = ','.join(m_list) + ','
+
     for key, value in d.items():
         if m in value[1]:
-            if m[-1] != "#" and m + "#" in value[1]:
-                value[1].replace(m[-2]+"#", "")
-                if m in value[1]:
-                    able.append([key, value[0], value[2]])
-            else:
-                able.append([key, value[0], value[2]])
+            able.append([key, value[0], value[2]])
 
-    print(d)
     able.sort(key=lambda x: (-x[1], x[2]))
-    print(able)
 
-
-    answer = ''
-    return answer
+    return "(None)" if len(able) == 0 else able[0][0]
 
 print(solution("ABCDEFG",  	["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
 print(solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]))
